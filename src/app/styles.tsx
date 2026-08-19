782af4e5359e5d9d2b178bee85514388d8109bc7
DSM4 <fatec-dsm4@academico.intra>
2026-08-18 22:54:56 -0300

    Style adicionado

---
+++ src/app/styles.tsx
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+export const extstyles = StyleSheet.create({
+    safeArea: {
+        flex: 1
+    },
+        container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    titulo:{
+        fontSize:32,
+        fontWeight: "bold",
+        color: '#e3217ff',
+    },
+    descricao:{
+        fontSize:18,
+        textAlign:"center",
+    marginTop: 15,
+    },
+})
